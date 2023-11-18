a629d617534ba896e603d8d1d78146ca3db74c1f
rohitsar <rohitsar@umich.edu>
2023-11-18 15:30:07 -0500

    header routes

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import Header from '../../components/header'
+import header from '@/components/header'
+
+export default function About() {
+  return (
+    <div>
+      <Header/>
+    </div>
+  )
+}
