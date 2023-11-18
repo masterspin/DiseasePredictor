b635558dda24a5d66f9c148baab8e8b976603229
rjutur <rjutur@umich.edu>
2023-11-18 17:24:29 -0500

    open ai

---
+++ src/app/moreinfo/page.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react';
+import openai from 'openai'; // Make sure to install openai package
+
+const MoreInfo = () => {
+  const [responseContent, setResponseContent] = useState<string>('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const API_KEY = 'API_KEY_HERE'; // Your OpenAI API key
+        openai.api_key = API_KEY;
+        openai.api_base = 'https://api.umgpt.umich.edu/azure-openai-api/ptu';
+        openai.api_version = '2023-03-15-preview';
+
+        const response = await openai.ChatCompletion.create({
+          engine: 'gpt-4',
+          messages: [
+            { role: 'system', content: 'You are a helpful assistant.' },
+            { role: 'user', content: 'Who won the world series in 2020?' },
+          ],
+        });
+
+        if (response && response.data && response.data.choices && response.data.choices.length > 0) {
+          const content = response.data.choices[0].message.content;
+          setResponseContent(content);
+        }
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>OpenAI Chat Response:</h1>
+      <p>{responseContent}</p>
+    </div>
+  );
+};
+
+export default MoreInfo;
