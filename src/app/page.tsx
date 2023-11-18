0e6322e2828d5cad363b6cd8b8e6d482a04f4a86
rjutur <rjutur@umich.edu>
2023-11-18 15:25:42 -0500

    routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Image from 'next/image'
 import Header from '../components/header'
-import header from '@/components/header'
+import Form from '@/components/form'
 
 export default function Home() {
   return (
     <div>
       <Header/>
+      <Form/>
     </div>
   )
 }
---
+++ src/components/form.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Link from 'next/link'
+// import * as ROUTES from "../constants/routes";
+import Image from 'next/image'
+
+type formProps = {
+    
+};
+
+const header:React.FC<formProps> = () => {
+    
+    return(
+        <div>
+            <fieldset>
+                <button>
+                    
+                </button>
+            </fieldset>
+        </div>
+    )
+}
+export default header;
