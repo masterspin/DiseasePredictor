0b402366b7aacd756f1e12a2761ce6a8c6eaa853
rohitsar <rohitsar@umich.edu>
2023-11-18 16:14:30 -0500

    added button and symptoms

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,33 +1,34 @@
 'use client'
 import React, { useState } from 'react';
 import Link from 'next/link'
+import { symptoms } from './symptoms';
 // import * as ROUTES from "../constants/routes";
 import Image from 'next/image'
 import { MultiSelect, MultiSelectProps } from '@uc-react-ui/multiselect';
-
 
 type formProps = {
     
 };
 
 const form:React.FC<formProps> = () => {
-
-    const [value, setValue] = useState(['desktop']);
+    async function handleOnSubmit(){
+        console.log(value)
+    }
+    const [value, setValue] = useState(['']);
     const  props: MultiSelectProps = {
-	label: 'Tags',
-    id: 'Id',
+	label: 'Select All Symptoms',
 	name: 'tags',
 	size: 'small',
-	optionList: [
-	],
+	optionList: symptoms,
 	placeholder: 'Add tags',
 	value: value,
 	valueChange: setValue
-  };
     
+  };
     return(
-        <div className="p-2">
+        <div className="p-2 w-1/2 flex">
 		    <MultiSelect {...props} />
+            <button className='bg-blue-500 hover:bg-blue-400 text-white font-bold px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded' onClick={handleOnSubmit}>Enter</button>
 	    </div>
     )
 }
