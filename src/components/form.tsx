d3c8e13387d7c81458d8739355910640b448ac20
rohitsar <rohitsar@umich.edu>
2023-11-19 02:04:51 -0500

    completed project

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -120,9 +120,12 @@ const form:React.FC<formProps> = () => {
                 </div>
             </div>
             </div>
-            <div className='mx-10' style={{ display: !hidden ? "block" : "none" }}>
-                Welcome to our Disease Predictor. To help us provide you with the most accurate predictions, please enter your symptoms in the box below. Describe how you're feeling using specific terms. For example, you might mention 'fever', 'headache', 'nausea', or any other sensations or observations you deem relevant. The more detailed your description, the better we can assist you. Please remember, this tool is for informational purposes only and should not replace professional medical advice.
-            </div>
+            <div className="max-w-md mx-auto bg-gray-100 border border-gray-300 p-6 rounded-lg shadow-md my-4" style={{ display: !hidden ? "block" : "none" }}>
+      <h2 className="text-lg font-semibold text-gray-800 mb-4">Welcome to our Disease Predictor</h2>
+      <p className="text-gray-700">
+        To help us provide you with the most accurate predictions, please enter your symptoms in the box below. Describe how you're feeling using specific terms. For example, you might mention 'fever', 'headache', 'nausea', or any other sensations or observations you deem relevant. The more detailed your description, the better we can assist you. Please remember, this tool is for informational purposes only and should not replace professional medical advice.
+      </p>
+    </div>
         </div>
     )
 }
