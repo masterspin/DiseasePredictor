b959e5dda4bb623cd6195b03079e3e0da265912f
rohitsar <rohitsar@umich.edu>
2023-11-19 01:19:18 -0500

    finished website

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -27,6 +27,9 @@ const form:React.FC<formProps> = () => {
 	valueChange: setValue
   };
   const [predictions, setPredictions] = useState(['']);
+  const [probabilities, setProbablitites] = useState(['']);
+  const [moreinfo, setMoreinfo] = useState(['']);
+  const [hidden, setHidden] = useState(false);
   async function handleOnSubmit(value:any) {
     let headers = new Headers();
     headers.append('Content-Type','application/json')
@@ -45,6 +48,9 @@ const form:React.FC<formProps> = () => {
         .then(data => {
           console.log(data.message);
           setPredictions(data.message)
+          setMoreinfo(data.responses)
+          setProbablitites(data.percentages)
+          setHidden(true)
         })
         .catch(error => {
           console.error('There was a problem with the fetch operation:', error);
@@ -54,48 +60,51 @@ const form:React.FC<formProps> = () => {
         <div className="p-2 ">
             <div className='justify-center flex items-center'>
 		    <MultiSelect {...props} />
-            <button className='bg-blue-500 hover:bg-blue-400 text-white font-bold px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded' onClick={() => handleOnSubmit(value)}>Enter</button>
+            <button className='bg-blue-500 hover:bg-blue-400 text-white font-bold px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded' disabled={value.length == 1} onClick={() => handleOnSubmit(value)}>Enter</button>
             </div>
-            <div>
+            <div style={{ display: hidden ? "block" : "none" }}>
             <div className="container mx-auto px-4 py-10">
                 <div className="text-center">
                     <h2 className="text-3xl font-semibold text-gray-800 mb-6">Your Potential Diagnosis Results</h2>
-                    <div className="mt-4">
+                    {/* <div className="mt-4">
                         <p className="text-lg text-gray-600">{predictions['2']}</p>
                         <p className="text-lg text-gray-600">{predictions['1']}</p>
                         <p className="text-lg text-gray-600">{predictions['0']}</p>
-                    </div>
+                    </div> */}
                 </div>
 
                 <div className="mt-12">
-                    <h3 className="text-2xl font-semibold text-gray-800 mb-4">Learn More About Each Condition</h3>
+                    {/* <h3 className="text-2xl font-semibold text-gray-800 mb-4">Learn More About Each Condition</h3> */}
                     
                     {/* Placeholder cards */}
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                         <div className="bg-white p-6 rounded-lg shadow hover:shadow-lg transition duration-300">
                             <div className="flex items-center">
-                                <Bs1Circle className="text-3xl text-blue-500"/>
+                                {/* <Bs1Circle className="text-3xl text-blue-500"/> */}
                                 <div className="ml-4">
-                                    <h4 className="text-xl font-semibold text-gray-800">{predictions['2']}</h4>
-                                    <p className="text-gray-600 mt-1">Brief description or summary...</p>
+                                <h4 className="text-xl font-semibold text-gray-800">1. {predictions['2']}</h4>
+                                    <h3 className="text-md italic text-gray-800">likelihood - {probabilities['2']}%</h3>
+                                    <p className="text-gray-600 mt-1">{moreinfo[2]}</p>
                                 </div>
                             </div>
                         </div>
                         <div className="bg-white p-6 rounded-lg shadow hover:shadow-lg transition duration-300">
                             <div className="flex items-center">
-                                <Bs2Circle className="text-3xl text-blue-500"/>
+                                {/* <Bs2Circle className="text-3xl text-blue-500"/> */}
                                 <div className="ml-4">
-                                    <h4 className="text-xl font-semibold text-gray-800">{predictions['1']}</h4>
-                                    <p className="text-gray-600 mt-1">Brief description or summary...</p>
+                                    <h4 className="text-xl font-semibold text-gray-800">2. {predictions['1']}</h4>
+                                    <h3 className="text-md italic text-gray-800">likelihood - {probabilities['1']}%</h3>
+                                    <p className="text-gray-600 mt-1">{moreinfo[1]}</p>
                                 </div>
                             </div>
                         </div>
                         <div className="bg-white p-6 rounded-lg shadow hover:shadow-lg transition duration-300">
                             <div className="flex items-center">
-                                <Bs3Circle className="text-3xl text-blue-500"/>
+                                {/* <Bs3Circle className="text-3xl text-blue-500"/> */}
                                 <div className="ml-4">
-                                    <h4 className="text-xl font-semibold text-gray-800">{predictions['0']}</h4>
-                                    <p className="text-gray-600 mt-1">Brief description or summary...</p>
+                                    <h4 className="text-xl font-semibold text-gray-800">3. {predictions['0']}</h4>
+                                    <h3 className="text-md italic text-gray-800">likelihood - {probabilities['0']}%</h3>
+                                    <p className="text-gray-600 mt-1">{moreinfo[0]}</p>
                                 </div>
                             </div>
                         </div>
