325f97b75193012c900d73e29588d95bf9642d4c
rjutur <rjutur@umich.edu>
2023-11-19 01:19:49 -0500

    discarding

--- flask/app.py
+++ flask/app.py
@@ -3,13 +3,66 @@ from flask_cors import CORS
 import joblib
 import ast
 import numpy as np
-import json 
+import json
+import os
+import openai
+import json
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route("/gpt", methods=['GET', 'POST'])
 def gptfunction():
+    openai.api_type = "azure"
+    openai.api_key = '4c5dd928da50479daa212d07285c952e'
+
+    openai.api_base = 'https://api.umgpt.umich.edu/azure-openai-api/ptu'
+    openai.api_version = '2023-03-15-preview'
+
+    try:
+        response = openai.ChatCompletion.create(
+            engine='gpt-4',
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": "Who won the world series in 2020?"}
+            ]
+        )
+
+        # print the response
+        print(response['choices'][0]['message']['content'])
+
+    except openai.error.APIError as e:
+        # Handle API error here, e.g. retry or log
+        print(f"OpenAI API returned an API Error: {e}")
+
+    except openai.error.AuthenticationError as e:
+        # Handle Authentication error here, e.g. invalid API key
+        print(f"OpenAI API returned an Authentication Error: {e}")
+
+    except openai.error.APIConnectionError as e:
+        # Handle connection error here
+        print(f"Failed to connect to OpenAI API: {e}")
+
+    except openai.error.InvalidRequestError as e:
+        # Handle connection error here
+        print(f"Invalid Request Error: {e}")
+
+    except openai.error.RateLimitError as e:
+        # Handle rate limit error
+        print(f"OpenAI API request exceeded rate limit: {e}")
+
+    except openai.error.ServiceUnavailableError as e:
+        # Handle Service Unavailable error
+        print(f"Service Unavailable: {e}")
+
+    except openai.error.Timeout as e:
+        # Handle request timeout
+        print(f"Request timed out: {e}")
+
+    except:
+        # Handles all other exceptions
+        print("An exception has occured.")
+
     return jsonify({
         'message' : "Hello, GPT!"
     }) 
--- flask/env/bin/activate.fish
+++ flask/env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/ritij/Documents/Projects/MHACKS16/flask/env"
+set -gx VIRTUAL_ENV "/Users/ritij/Documents/Projects/DiseasePredictor/flask/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
