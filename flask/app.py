59d530ceaa319cec3f3bdf1034cac140c6f7639e
rohitsar <rohitsar@umich.edu>
2023-11-18 22:41:32 -0500

    got fetch to work

--- flask/app.py
+++ flask/app.py
@@ -1,19 +1,38 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
+import joblib
+import ast
+import numpy as np
+import json 
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/gpt", methods=['GET'])
+@app.route("/gpt", methods=['GET', 'POST'])
 def gptfunction():
     return jsonify({
         'message' : "Hello, GPT!"
     }) 
 
-@app.route("/model", methods=['GET'])
+@app.route("/model", methods=['GET', 'POST'])
 def modelfunction():
+    dict_str = request.data.decode('UTF-8')
+    input_values = ast.literal_eval(dict_str)['feature_matrix']
+    input_values = input_values[1:]
+    feature_matrix = np.zeros((1,131))
+    model = joblib.load('forest.joblib')
+    features = model.feature_names_in_
+    classes = model.classes_
+    word_to_idx = {word.lower() : idx for idx, word in enumerate(features)}
+    for input in input_values:
+        feature_matrix[0,word_to_idx[input.replace(' ', '_')]] = 1
+    scores = model.predict_proba(feature_matrix)
+    scores = scores.argsort()
+    predictions = scores[:,-3:]
+    predictions = classes[predictions]
+    message = {idx: word for idx, word in enumerate(predictions[0])}
     return jsonify({
-        'message' : "Hello, MODEL!"
+        'message' : message
     }) 
 
 if __name__ == "__main__":
